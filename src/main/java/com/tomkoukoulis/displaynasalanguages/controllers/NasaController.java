2e1e1b75e542ab65745545911c84f4115d2976e5
tomkoukoulis <tomkoukoulis@toms-imac.lan>
2017-01-19 21:23:50 +0000

    Implemented controller classes

--- src/main/java/com/tomkoukoulis/displaynasalanguages/controllers/NasaController.java
+++ src/main/java/com/tomkoukoulis/displaynasalanguages/controllers/NasaController.java
@@ -9,14 +9,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.client.RestClientException;
 
-
 @Controller
 public class NasaController extends Restful {
 
     public static final String GITHUB_LIST_NASA_REPOSITORIES_URL = "https://api.github.com/orgs/NASA/repos";
     public static final String GITHUB_LIST_REPOSITORY_LANGUAGES_1 = "https://api.github.com/repos/NASA/";
     public static final String GITHUB_LIST_REPOSITORY_LANGUAGES_2 = "/languages";
-    
+
     public static String getNasaRepositoryLanguagesUrl(String name) {
         return GITHUB_LIST_REPOSITORY_LANGUAGES_1 + name + GITHUB_LIST_REPOSITORY_LANGUAGES_2;
     }
@@ -24,20 +23,49 @@ public class NasaController extends Restful {
     @RequestMapping(value = "/languages", method = RequestMethod.GET)
     public String viewLanguages(ModelMap model) {
 
+        try {
+            model.addAttribute("languages", getLanguages());
+        } catch (RestClientException rce) {
+            model.addAttribute("error-message", "There was a problem communicating with GitHub.");
+        } catch (NumberFormatException nfe) {
+            model.addAttribute("error-message", "The data received from GitHub had an error.");
+        }
+
         return "index";
     }
 
     protected HashMap<String, Long> getLanguages() throws RestClientException, NumberFormatException {
-        
-        return null;
+        Repository[] nasaRepos = getRepositories();
+
+        HashMap<String, Long> collatedLanguages = new HashMap<>();
+
+        for (int i = 0; i < 2; i++) { //TODO: Replace 2 with nasaRepos.length
+            String languagesUrl = getNasaRepositoryLanguagesUrl(nasaRepos[i].getName());
+
+            Languages repoLanguages = get(languagesUrl, Languages.class);
+
+            HashMap languages = repoLanguages.get();
+
+            collateLanguages(languages, collatedLanguages);
+        }
+
+        return collatedLanguages;
     }
 
     protected Repository[] getRepositories() throws RestClientException {
-        
-        return null;
+        return get(GITHUB_LIST_NASA_REPOSITORIES_URL, Repository[].class);
     }
 
     protected void collateLanguages(HashMap<String, String> languages, HashMap<String, Long> collatedLanguages) throws NumberFormatException {
+        for (HashMap.Entry<String, String> entry : languages.entrySet()) {
+            String language = entry.getKey();
+            String noOfLines = entry.getValue();
 
+            if (collatedLanguages.get(language) == null) {
+                collatedLanguages.put(language, Long.parseLong(noOfLines));
+            } else {
+                collatedLanguages.put(language, collatedLanguages.get(language) + Long.parseLong(noOfLines));
+            }
+        }
     }
 }
--- src/main/java/com/tomkoukoulis/displaynasalanguages/controllers/Restful.java
+++ src/main/java/com/tomkoukoulis/displaynasalanguages/controllers/Restful.java
@@ -1,16 +1,29 @@
 package com.tomkoukoulis.displaynasalanguages.controllers;
 
+import java.util.ArrayList;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 public class Restful {
-        
-    protected RestTemplate getRestTemplate() {
 
-        return null;
+    private RestTemplate restTemplate;
+
+    protected RestTemplate getRestTemplate() {
+        if (restTemplate != null) {
+            return restTemplate;
+        } else {
+            ArrayList<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
+            messageConverters.add(new MappingJackson2HttpMessageConverter());
+            restTemplate = new RestTemplate();
+            restTemplate.setMessageConverters(messageConverters);
+
+            return restTemplate;
+        }
     }
 
     protected <T> T get(String url, Class<T> aClass) throws RestClientException {
-        return null;
+        return aClass.cast(getRestTemplate().getForObject(url, aClass));
     }
 }
